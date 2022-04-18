93be4076155f6213e6c003ffe8654d7a95f7c5e9
olszewskik <kamil.web@gmail.com>
2022-04-18 21:16:26 +0200

    setup database

--- app/db/base.py
+++ app/db/base.py
@@ -1,1 +1,3 @@
-
+from app.db.session import Base
+from app.models.item import Item
+from app.models.user import User
--- app/db/session.py
+++ app/db/session.py
@@ -3,7 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine(
-    "sqlite:///booking_system_database.db", connect_args={"check_same_thread": False}
+    "sqlite:///app/db/booking_system_database.db",
+    connect_args={"check_same_thread": False},
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
